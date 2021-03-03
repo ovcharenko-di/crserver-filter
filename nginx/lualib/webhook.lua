c24f8dd3f58fd189d85d83d8206b88be4c154fe7
Andrey Ovsiankin <ovsiankin.a.a@gmail.com>
2021-03-03 13:32:34 +0300

    Сделал плагин коммит-хука

--- nginx/lualib/webhook.lua
+++ nginx/lualib/webhook.lua
@@ -5,27 +5,37 @@
 -- Основан на работе https://github.com/asosnoviy/commitHook
 
 -- TODO 
--- * переписать под resty.http вместо luacurl https://github.com/ledgetech/lua-resty-http
 -- * обеспечить единоразовое чтение request_body всеми плагинами, а не каждым по отдельности
 
+local _M = {}
 
+function call(url, body)
 
--- local data = ngx.var.request_body
-        
---                if string.find(data, "DevDepot_commitObjects") then
-               
---                 local cURL = require("luacurl")
---                 local api_http = "http://192.168.10.144"
---                 local api_key = "123"
---                 local api_paste = "testapi"
---                 c = cURL.new()
---                 c:setopt(curl.OPT_URL, api_http)
---                 c:setopt(curl.OPT_POST, true)
---                 c:setopt(curl.OPT_POSTFIELDS, "api_option=paste&api_dev_key="..api_key.."&api_paste_code="..api_paste.."&api_paste_private=0&api_paste_format=php")
---                 c:setopt(curl.OPT_TRANSFERTEXT, true)
---                 c:setopt(curl.OPT_VERBOSE, true)
---                 c:setopt(curl.OPT_NOBODY, false)
---                 c:perform()
---                 c:close()
---               end
---             ';
+    local data = ngx.var.request_body
+
+    ngx.req.read_body()
+
+    local req = ngx.var.request_body
+    if req == nil then
+        return
+    end
+
+    if string.find(req, "DevDepot_commitObjects") then
+        local http = require "resty.http"
+        local httpc = http.new()
+        local res, err = httpc:request_uri(url, {
+            method = "POST",
+            body = body,
+            headers = {
+            ["Content-Type"] = "application/x-www-form-urlencoded",
+            },
+            keepalive_timeout = 30000,
+            keepalive_pool = 10
+        })
+
+        if not res then
+            ngx.log(ngx.WARN, "error calling "..url)
+            return
+        end
+    end
+end
